ef4ffcb02f95d94b78f52e72103b849099ecf52b
Furkan Guner <furkan-guner@hotmail.com>
2020-07-27 13:30:53 +0300

    Add project files.

---
+++ RNCBook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using RNCBook.DataAccess.Data;
+using RNCBook.DataAccess.Repository.IRepository;
+using RNCBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RNCBook.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s=>s.Id == category.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ RNCBook.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,28 @@
+﻿using RNCBook.DataAccess.Data;
+using RNCBook.DataAccess.Repository.IRepository;
+using RNCBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RNCBook.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(CoverType coverType)
+        {
+            var objFromDb = _db.CoverTypes.FirstOrDefault(s => s.Id == coverType.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = coverType.Name;
+            }
+        }
+    }
+}

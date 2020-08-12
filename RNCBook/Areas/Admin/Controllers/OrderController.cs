d7726217e31477a58ba81094c74456313612429d
Furkan Guner <furkan-guner@hotmail.com>
2020-08-12 18:31:50 +0300

    Added Order Management

---
+++ RNCBook/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RNCBook.DataAccess.Repository.IRepository;
+using RNCBook.Models;
+using RNCBook.Models.ViewModels;
+using RNCBook.Utility;
+using Stripe;
+
+namespace RNCBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        [BindProperty]
+        public OrderDetailsVM OrderVM { get; set; }
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            OrderVM = new OrderDetailsVM()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(x => x.Id == id,
+                                                                        includeProperties: "ApplicationUser"),
+                OrderDetails = _unitOfWork.OrderDetails.GetAll(x => x.OrderId == id, includeProperties: "Product")
+            };
+            return View(OrderVM);
+        }
+
+        [Authorize(Roles =SD.Role_Admin+","+SD.Role_Employee)]
+        public IActionResult StartProcessing(int id)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(x => x.Id == id);
+            orderHeader.OrderStatus = SD.StatusInProcess;
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult ShipOrder(int id)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(x => x.Id == OrderVM.OrderHeader.Id);
+            orderHeader.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+            orderHeader.Carrier = OrderVM.OrderHeader.Carrier;
+            orderHeader.OrderStatus = SD.StatusShipped;
+            orderHeader.ShippingDate = DateTime.Now;
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult CancelOrder(int id)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(x => x.Id == id);
+            if(orderHeader.OrderStatus == SD.StatusApproved)
+            {
+                var options = new RefundCreateOptions
+                {
+                    Amount = Convert.ToInt32(orderHeader.OrderTotal * 100),
+                    Reason = RefundReasons.RequestedByCustomer,
+                    Charge = orderHeader.TransactionId
+                };
+                var service = new RefundService();
+                Refund refund = service.Create(options);
+
+                orderHeader.OrderStatus = SD.StatusRefunded;
+                orderHeader.PaymentStatus = SD.StatusRefunded;
+            }
+            else
+            {
+                orderHeader.OrderStatus = SD.StatusCancelled;
+                orderHeader.PaymentStatus = SD.StatusCancelled;
+            }
+            orderHeader.OrderStatus = SD.StatusInProcess;
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetOrderList(string status)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            IEnumerable<OrderHeader> orderHeaderList;
+            if(User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+            {
+                orderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            }
+            else
+            {
+                orderHeaderList = _unitOfWork.OrderHeader.GetAll(x => x.ApplicationUserId == claim.Value ,includeProperties: "ApplicationUser");
+            }
+            switch (status)
+            {
+                case "pending":
+                    orderHeaderList = orderHeaderList.Where(x => x.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaderList = orderHeaderList.Where(x => x.OrderStatus == SD.StatusApproved ||
+                                                             x.OrderStatus==SD.StatusInProcess || 
+                                                             x.OrderStatus==SD.StatusPending);
+                    break;
+                case "completed":
+                    orderHeaderList = orderHeaderList.Where(x => x.OrderStatus == SD.StatusShipped);
+                    break;
+                case "rejected":
+                    orderHeaderList = orderHeaderList.Where(x => x.OrderStatus == SD.StatusCancelled ||
+                                                              x.OrderStatus == SD.StatusRefunded ||
+                                                              x.OrderStatus == SD.PaymentStatusRejected);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = orderHeaderList });
+        }
+        #endregion
+    }
+}
--- RNCBook/Areas/Customer/Controllers/CartController.cs
+++ RNCBook/Areas/Customer/Controllers/CartController.cs
@@ -184,7 +184,7 @@ namespace RNCBook.Areas.Customer.Controllers
             ShoppingCartVM.OrderHeader.OrderDate = DateTime.Now;
             _unitOfWork.OrderHeader.Add(ShoppingCartVM.OrderHeader);
             _unitOfWork.Save();
-            List<OrderDetails> orderDetailsList = new List<OrderDetails>();
+
             foreach(var item in ShoppingCartVM.ListCart)
             {
                 item.Price = SD.GetPriceBaseOnQuantity(item.Count, item.Product.Price, item.Product.Price50, item.Product.Price100);
